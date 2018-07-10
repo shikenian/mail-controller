480e7e9ffcce87cce0d0cd11aa3dc811fde0b98f
Kenian Shi <shikenian@126.com>
2018-07-10 18:21:48 +0800

    first commit

---
+++ src/main/java/com/tools/mailcontroller/MailControllerApplication.java
@@ -0,0 +1,12 @@
+package com.tools.mailcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailControllerApplication.class, args);
+    }
+}
